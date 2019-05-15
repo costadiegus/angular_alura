0ebd74b55be1753016ccde9b187504e24c8b62c2
DG Costa <costadiegus@gmail.com>
2019-05-14 23:38:36 -0300

    Angular Parte 01 Aula 02.04

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'alurapic';
-  description = 'AngularJS Icon';
-  url = 'https://www.pinclipart.com/picdir/big/206-2069911_icon-angularjs-issue-angular-icon-clipart.png';
-}
+export class AppComponent {}
