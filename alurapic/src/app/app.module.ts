32fe92d350eca97a67ae5b9ff7b176110766feb2
DG Costa <costadiegus@gmail.com>
2019-05-17 00:54:48 -0300

    Angular Parte 01 Aula 03.06

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -12,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     PhotosModule
   ],
   providers: [],
