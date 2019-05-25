91c17788b2656660c5e913b761c98939b2786fa0
DG Costa <costadiegus@gmail.com>
2019-05-25 09:26:08 -0300

    Angular Parte 01 Aula 06.10

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Photo } from '../photo/photo';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -15,10 +16,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
 
     this.debounce
@@ -30,4 +38,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
   }
+
+  load() {
+    this.photoService
+        .listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+            this.photos = this.photos.concat(photos);
+            if (!photos.length) {
+              this.hasMore = false;
+            }
+        });
+  }
+
 }
