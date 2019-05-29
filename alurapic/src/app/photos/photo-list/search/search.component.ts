d6e472e99e03cb84919729e5646c2ded5be3ee27
DG Costa <costadiegus@gmail.com>
2019-05-29 01:44:59 -0300

    Angular Parte 01 Aula 07.07

--- alurapic/src/app/photos/photo-list/search/search.component.ts
+++ alurapic/src/app/photos/photo-list/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -9,13 +9,16 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
+  @Output() myOnTyping = new EventEmitter<string>();
+  @Input() value = '';
   debounce: Subject<string> = new Subject<string>();
 
   constructor() { }
 
   ngOnInit(): void {
     this.debounce
-    .pipe(debounceTime(300));
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.myOnTyping.emit(filter));
   }
 
   ngOnDestroy(): void {
