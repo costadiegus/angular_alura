078c79f6c5adafa9a77823abde5f312e07729fac
DG Costa <costadiegus@gmail.com>
2019-06-22 22:53:19 -0300

    Angular Parte 02 Aula 01.04

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
 }
